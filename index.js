87f79a4bcc62c971f5feede3a7165d3ba8d612f5
Hunter <farmerhunter@proton.me>
2023-02-22 23:19:38 -0500

    added ready and interactions

--- index.js
+++ index.js
@@ -32,6 +32,25 @@ client.on(Events.InteractionCreate, interaction => {
 	console.log(interaction);
 });
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
